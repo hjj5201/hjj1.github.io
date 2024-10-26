b4ea0c9eb88029927520736f02bb6d5c71373f19
hjj5201 <1703236513@qq.com>
2024-10-26 15:59:15 +0800

    将项目列表页面JS改造成TS

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,24 +1,26 @@
 import { useEffect, useState } from "react";
 
-export const isFalsy = (value) => value === 0 ? false : !value
-export const cleanObject = (object) =>{
+export const isFalsy = (value:any) => value === 0 ? false : !value
+export const cleanObject = (object:object) =>{
     const result = {...object}
     Object.keys(result).forEach(key => {
+        //@ts-ignore
         const value = result[key]
         if(isFalsy(value)){
+            //@ts-ignore
             delete result[key]
         }
     });
     return result
 }
 // 写自定义HOOK时，一定要加use，不然就是普通函数而用不了其他hook比如生命钩子
-export const useMount = (callback) =>{
+export const useMount = (callback:() => void) =>{
     useEffect(() =>{
         callback()
     },[])
 }
 
-export const useDebounce = (value,delay) =>{
+export const useDebounce = (value:any,delay?:number) =>{
     const [debounceValue,setDebounceValue] = useState(value)
 
     useEffect(()=>{
