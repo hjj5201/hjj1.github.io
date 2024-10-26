c7136dd0fbd9648c9bad9d81b981e6ecb7a8e716
hjj5201 <1703236513@qq.com>
2024-10-26 15:18:44 +0800

    自定义HOOK

--- src/screens/project-list/index.jsx
+++ src/screens/project-list/index.jsx
@@ -2,7 +2,7 @@ import React from "react"
 import { List } from "./list"
 import { SearchPanel } from "./search-panel"
 import { useState,useEffect } from "react"
-import { cleanObject } from "utils"
+import { cleanObject, useMount,useDebounce } from "utils"
 import *as qs from "qs"
 
 const apiUrl = process.env.REACT_APP_API_URL
@@ -14,21 +14,22 @@ export const ProjectListScreen = () =>{
         name:'',
         personId:''
     })
+    const debouncedParam = useDebounce(param,200)
     useEffect(()=>{
-        fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(param))}`).then(async response=>{
+        fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(debouncedParam))}`).then(async response=>{
             if(response.ok){
                 setList(await response.json())
             }
         })
-    },[param])
+    },[debouncedParam])
 
-    useEffect(()=>{
+    useMount(()=>{
         fetch(`${apiUrl}/users`).then(async response=>{
             if(response.ok){
                 setUsers(await response.json())
             }
         })
-    },[])
+    })
     return <div>
         <SearchPanel param={param} setParam={setParam} users={users}/>
         <List list={list} users={users}/>
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react";
+
 export const isFalsy = (value) => value === 0 ? false : !value
 export const cleanObject = (object) =>{
     const result = {...object}
@@ -9,6 +11,21 @@ export const cleanObject = (object) =>{
     });
     return result
 }
+// 写自定义HOOK时，一定要加use，不然就是普通函数而用不了其他hook比如生命钩子
+export const useMount = (callback) =>{
+    useEffect(() =>{
+        callback()
+    },[])
+}
 
-    
+export const useDebounce = (value,delay) =>{
+    const [debounceValue,setDebounceValue] = useState(value)
+
+    useEffect(()=>{
+        const timeout = setTimeout(() => setDebounceValue(value),delay)
+        return ()=>clearTimeout(timeout)
+    },[value,delay])
+    return debounceValue
+}
+
 
