8e55395dae5851cb541f61bdad626afccdf5c33c
hjj5201 <1703236513@qq.com>
2024-10-28 12:16:23 +0800

    用Row布局页面

--- src/authenticater-app.tsx
+++ src/authenticater-app.tsx
@@ -1,5 +1,6 @@
 //非登录注册页面
 import styled from "@emotion/styled"
+import { Row } from "components/lib"
 import { useAuth } from "context/auth-context"
 import React from "react"
 import { ProjectListScreen } from "screens/project-list"
@@ -20,11 +21,12 @@ export const AuthenticaterApp = () =>{
     const {logout} = useAuth()
     return <div>
         <Container>
-            <Header>
-                <HeaderLeft>
-                    <h3>Logo</h3>
-                    <h3>项目</h3>
-                    <h3>用户</h3>
+            <Header between={true}>
+                {/* 给lib传props */}
+                <HeaderLeft gap={true}>
+                    <h2>Logo</h2>
+                    <h2>项目</h2>
+                    <h2>用户</h2>
                 </HeaderLeft>
                 <HeaderRight>
                     <button onClick={logout}>登出</button>
@@ -37,22 +39,20 @@ export const AuthenticaterApp = () =>{
     </div>
 }
 
+const HeaderItem = styled.h3`
+    margin-right: 3rem;
+`
+
 const Container = styled.div`
     /* display: grid; */
     grid-template-rows: 6rem 1fr 6rem;
     height: 100vh;
 `
 // grid-area是用来给grid子元素起名字的
-const Header = styled.header`
-grid-area:header;
-display: flex;
-flex-direction: row;
-align-items: center;
-justify-content: space-between;
+const Header = styled(Row)`
 `
-const HeaderLeft = styled.div`
-display: flex;
-align-items: center;
+//引入lib的css变量
+const HeaderLeft = styled(Row)`
 `
 const HeaderRight = styled.div``;
 const Main = styled.main`grid-area:main`;
---
+++ src/components/lib.tsx
@@ -0,0 +1,20 @@
+import styled from "@emotion/styled";
+
+//代码复用
+export const Row = styled.div<{
+    gap?:number|boolean,
+    between?:boolean
+    marginbottom?:number
+}>`
+    display: flex;
+    align-items: center;
+    justify-content: ${props => props.between ? 'space-between' : undefined};
+    margin-bottom: ${props => props.marginbottom + 'rem'};
+//给他子元素设置样式  最高优先级别后面的东西代表  防止子元素不垂直居中 
+>* {
+    margin-top: 0 !important;
+    margin-bottom: 0 !important;
+    /* 每个直接子元素都要有个向右的距离，项目里的 */
+    margin-right: ${props => typeof props.gap === 'number' ? props.gap + 'rem': props.gap ? '2rem' : undefined};
+}
+`
