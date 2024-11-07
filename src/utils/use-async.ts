7c89bf79cbea5810f417c91c8baa3d73cc736656
hjj5201 <1703236513@qq.com>
2024-11-07 19:38:06 +0800

    改造代码

--- src/utils/use-async.ts
+++ src/utils/use-async.ts
@@ -1,5 +1,5 @@
 import { error } from "console";
-import { useCallback, useState } from "react";
+import { useCallback, useReducer, useState } from "react";
 import { useMemo } from "react";
 import { useMountedRef } from "utils";
 
@@ -19,20 +19,21 @@ const defaultConfig = {
     throwOnError:false
 }
 
+// 一些判断可以在抽象一层
+const useSafeDispatch = <T>(dispatch:(...args:T[]) => void) => {
+    const mountedRef = useMountedRef()
+    return useCallback((...args:T[]) => (mountedRef.current ? dispatch(...args):void 0),[dispatch,mountedRef] )
+}
+
 export const useAsync = <D>(initialState?: State<D>,initalConfig?:typeof defaultConfig) =>{
     const config ={...defaultConfig,...initalConfig}
-    const memoizedInitialState = useMemo(() => ({
-        ...defaultInitialState,
-        ...initialState
-    }), [initialState]);
-    const [state,setState] = useState<State<D>>(
-        //     {
-        //     ...defaultInitialState,
-        //     ...initialState,
-        // }
-        memoizedInitialState
+    const [state,dispatch] = useReducer((state:State<D>,action:Partial<State<D>>) => ({...state,...action}),
+            {
+            ...defaultInitialState,
+            ...initialState,
+        }
 )
-    const mountedRef = useMountedRef()
+    const safeDispatch = useSafeDispatch(dispatch)
     // useState直接传入函数的含义是：惰性初始化；所以，要用useState保存函数，不能直接传入函数
     // https://codesandbox.io/s/blissful-water-230u4?file=/src/App.js
     // 套俩层为了存储函数
@@ -40,17 +41,17 @@ export const useAsync = <D>(initialState?: State<D>,initalConfig?:typeof default
 
     })
 
-    const setData = useCallback( (data:D) => setState ({
+    const setData = useCallback( (data:D) => safeDispatch({
         data,
         stat:'success',
         error:null
-    }),[])
+    }),[safeDispatch])
 
-    const setError = useCallback((error:Error) => setState({
+    const setError = useCallback((error:Error) => safeDispatch({
         error,
         stat:'error',
         data:null
-    }),[])
+    }),[safeDispatch])
     //用来触发异步请求  useCallBack 是用于函数的特殊的useMemo 也是用来控制循环依赖的
     const run =  useCallback((promise : Promise<D>,runConfig?:{retry: ()=>Promise<D>})=> {
         if(!promise || !promise.then) {
@@ -63,13 +64,11 @@ export const useAsync = <D>(initialState?: State<D>,initalConfig?:typeof default
             }
         })
         // setState函数式用法
-        setState(prevState => ({...prevState,stat:'loading'}))
+        safeDispatch({stat:'loading'})
         return promise.then(data =>{
-            //增加判断,当页面挂载成功后,才设置data
+            //增加判断,当页面挂载成功后,才设置data  抽象之后吧判断丢给抽象函数
             // 不这样的话，当promise在加载的时候页面被挂载，但是异步任务还在进行就会导致data依旧设置导致报错
-            if(mountedRef.current){
                 setData(data)
-            }
             return data
         }).catch(error =>{
             //catch会消化异常,如果不主动抛出，外面是接受不到异常的 就不会显示账号密码错误这类异常
@@ -78,7 +77,7 @@ export const useAsync = <D>(initialState?: State<D>,initalConfig?:typeof default
             if(config.throwOnError) return Promise.reject(error)
             return Promise.reject(error)
         })
-    },[config.throwOnError,mountedRef,setData,setError])
+    },[config.throwOnError,setData,setError,safeDispatch])
     return {
         isIdle : state.stat === 'idle',
         isLoading : state.stat === 'loading',
--- src/utils/use-undo.ts
+++ src/utils/use-undo.ts
@@ -1,45 +1,41 @@
-import { useCallback, useState } from "react"
+import { useCallback, useReducer, useState } from "react"
 
-export const useUndo = <T>(initialPresent: T) => {
-    // 记录历史操作的合集
-    // const [past,setPast] = useState<T[]>([])
-    // const [present,setPresent] = useState(initialPresent)
-    // const [future,setFuture] = useState<T[]>([])
+const UNDO =  'UNDO'
+const REDO = 'REDO'
+const SET = 'SET'
+const RESET = 'RESET'
 
+type State<T> = {
+    past:T[],
+    present:T,
+    future:T[]
+}
 
-    //合并状态。防止useCallBack依赖项过多重复 把互相关联，互相影响的状态结合在一起 这样子做久useCallBack不需要再增加依赖
-    const [state,setState] = useState({
-        past:[] as T[],
-        present:initialPresent as T,
-        future:[] as T[],
-    })
+type Action<T> = {newPresent?: T,type:typeof UNDO | typeof REDO | typeof SET | typeof RESET} 
 
-    // 有历史记录
-    const canUndo = state.past.length !==0
-    const canRedo = state.future.length ! == 0
+const  undoReducer = <T>(state:State<T>,action:Action<T>) => {
+    const {past,present,future} = state 
+    const {type,newPresent} = action
 
-    const Undo = useCallback(() => {
-        setState(currentState => {
-            const {past,present,future} = currentState
-
-            if(past.length === 0 ) return currentState
-
-            const previous = past[past.length -1]
-            // 复制从0下标到...下标
-            const newPast = past.slice(0,past.length-1)
+    switch(action.type) {
+        case UNDO : {
+                const {past,present,future} = state
     
-            return {
-                past:newPast,
-                present:previous,
-                future:[present,...future]
-            }
-        })
-    },[])
-
-    const redo = useCallback(() => {
-        setState(currentState =>{
-            const {past,present,future} = currentState
-            if(future.length === 0) return currentState
+                if(past.length === 0 ) return state
+    
+                const previous = past[past.length -1]
+                // 复制从0下标到...下标
+                const newPast = past.slice(0,past.length-1)
+        
+                return {
+                    past:newPast,
+                    present:previous,
+                    future:[present,...future]
+                }
+        }
+        case REDO: {
+            const {past,present,future} = state
+            if(future.length === 0) return state
 
             const next = future[0]
             const newFuture =future.slice(1)
@@ -49,33 +45,55 @@ export const useUndo = <T>(initialPresent: T) => {
                 present:next,
                 future:newFuture,
             }
-        })
-    }
-,[])
-    const set =useCallback((newPresent : T) => {
-        setState(currentState => {
-            const {past,present,future} = currentState
+        }
+
+        case SET: {
+            const {past,present,future} = state
             if(newPresent === present) {
-                return currentState
+                return state
             }
             return {
                 past:[...past,present],
                 present:newPresent,
                 future:[],
             }
-        })
-    },[])
+        }
 
-
-    const reset =useCallback( (newPresent:T) => {
-        setState(currentState => {
+        case RESET: {
             return {
                 past:[],
                 present:newPresent,
                 future:[]
             }
-        })
-    },[])
+        }
+    }
+    return state
+}
+
+export const useUndo = <T>(initialPresent: T) => {
+    // 记录历史操作的合集
+    // const [past,setPast] = useState<T[]>([])
+    // const [present,setPresent] = useState(initialPresent)
+    // const [future,setFuture] = useState<T[]>([])
+
+    const [state,dispath] = useReducer(undoReducer,{
+        past:[] ,
+        present:initialPresent,
+        future:[] ,
+    }as State<T>)
+
+
+    // 有历史记录
+    const canUndo = state.past.length !==0
+    const canRedo = state.future.length ! == 0
+
+    const Undo = useCallback(() => dispath({type:UNDO}),[])
+
+    const redo = useCallback(() => dispath({type:REDO}) ,[])
+    const set =useCallback((newPresent : T) => dispath({type:SET,newPresent}),[])
+
+
+    const reset =useCallback( (newPresent:T) => dispath({type:RESET,newPresent}),[])
 
     return [
         state,
