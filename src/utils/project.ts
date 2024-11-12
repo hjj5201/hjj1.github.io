af9ce5393e37834057d7725c9d37e58d0fd15ac8
hjj5201 <1703236513@qq.com>
2024-11-12 21:09:27 +0800

    乐观更新初体验

--- src/utils/project.ts
+++ src/utils/project.ts
@@ -4,6 +4,7 @@ import { useCallback, useEffect } from "react";
 import { cleanObject } from "utils";
 import { useHttp } from "./http";
 import { useMutation, useQuery, useQueryClient } from "react-query";
+import { useProjectsSearchParams } from "screens/project-list/util";
 // 它的主要功能是使用异步请求来获取项目列表，同时管理加载状态和错误处理。
 export const useProjects = (param? : Partial<Project>) =>{
     const client = useHttp()
@@ -16,11 +17,27 @@ export const useProjects = (param? : Partial<Project>) =>{
 export const useEditProject = () =>{
     const client = useHttp()
     const queryClient = useQueryClient()
+    const [searchParams] = useProjectsSearchParams()
+    const queryKey = ['projects',searchParams]
     return useMutation((param:Partial<Project>) => client(`projects/${param.id}`,{
         data:param,
         method:"PATCH",
     }),{
-        onSuccess: () => queryClient.invalidateQueries('projects') 
+        onSuccess: () => queryClient.invalidateQueries(queryKey),
+        // 乐观更新，为了点击收藏，不等服务器返回过来就直接更新，如果服务器出错就回滚
+        async onMutate(target) {
+            
+            const previousItems = queryClient.getQueryData(queryKey)
+            // old缓存的列表数据
+            queryClient.setQueryData(queryKey,(old?:Project[]) => {
+                return old?.map(project => project.id === target.id ? {...project,...target} : project) || [] 
+            })
+            return {previousItems}
+        },
+        onError(error,newItem,context : any) {
+            //回滚机制  context有previousItem返回过来的  出错之后把原来的数据回滚掉
+            queryClient.setQueryData(queryKey,context.previousItems)
+        }
     }) 
   
 }
